49207d527b848dbbaa6c69d3eef6f43ca24273ba
JooHw <rkadbwkd@nate.com>
2023-02-14 22:50:19 +0900

    Member domain run and Test

--- src/main/java/hello/core/member/MemberSerivceImpl.java
+++ src/main/java/hello/core/member/MemberSerivceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberSerivceImpl implements MemberService{
 
+    // MemberServiceImpl은 MemberRepository , MemoryMember Repository 모두 의존
     private final MemberRepository memberRepository = new MemoryMemberRepository(); // CTRL + SHIFT + ENTFER
 
 
