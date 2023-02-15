af394bd44cf24649cebaa3aebd3427ae758fa1aa
JooHw <rkadbwkd@nate.com>
2023-02-15 23:36:28 +0900

    appConfig refactor

--- src/main/java/hello/core/Appconfig.java
+++ src/main/java/hello/core/Appconfig.java
@@ -2,22 +2,34 @@ package hello.core;
 
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberSerivceImpl;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 
 public class Appconfig{
     public MemberService memberService(){
-        return new MemberSerivceImpl(new MemoryMemberRepository());
+        return new MemberSerivceImpl(memberRepository());
     }
 
+    // ctrl + alt + m
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
 
     }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+
 
 }
